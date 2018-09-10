316a0dc3dbca897560aba1fe221d90e9a29b9941
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 09:13:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,6 +11,7 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
