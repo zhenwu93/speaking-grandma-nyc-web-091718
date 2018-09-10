623080091ae098f8f2d30a2264a36dfd2aa2503f
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 09:17:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,5 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  else
-    return "HUH?! SPEAK UP, SONNY!"
+
   end
